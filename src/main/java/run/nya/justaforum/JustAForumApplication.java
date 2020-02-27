8a37a74c9591993221929265d1cf57528525c574
HAIZAKURA <hhbilly99@gmail.com>
2020-02-27 12:31:57 +0800

    Rename main class

--- src/main/java/run/nya/justaforum/JustAForumApplication.java
+++ src/main/java/run/nya/justaforum/JustAForumApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ForumDemoApplication {
+public class JustAForumApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ForumDemoApplication.class, args);
+        SpringApplication.run(JustAForumApplication.class, args);
     }
 
 }
