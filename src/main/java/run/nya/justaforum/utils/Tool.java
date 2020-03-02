da4bb8d1bad09a6225f652eb65e4ce29a585fee4
HAIZAKURA <hhbilly99@gmail.com>
2020-03-02 11:27:48 +0800

    combined Time and MD5 into Tool

--- src/main/java/run/nya/justaforum/utils/Tool.java
+++ src/main/java/run/nya/justaforum/utils/Tool.java
@@ -2,8 +2,10 @@ package run.nya.justaforum.utils;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class MD5 {
+public class Tool {
 
     /**
      *
@@ -31,6 +33,22 @@ public class MD5 {
             e.printStackTrace();
             return null;
         }
+    }
+
+    /**
+     *
+     * @name getNowTime
+     * @mark 获取当前时间
+     * @return
+     *
+     */
+    public static String getNowTime() {
+        String res = null;
+        SimpleDateFormat sdf = new SimpleDateFormat();
+        sdf.applyPattern("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date();
+        res = sdf.format(date);
+        return res;
     }
 
 }
